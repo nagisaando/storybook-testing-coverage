daddd983566aa074fa10a7c9615c5d059d95c11d
Nagisa Ando <nagisa@hanayoku.com>
2022-08-02 13:59:51 -0700

    first commit

---
+++ .storybook/main.cjs
@@ -0,0 +1,34 @@
+const istanbul = require('vite-plugin-istanbul');
+// get default config from addon-coverage – no need to register it
+const constants = require('@storybook/addon-coverage/dist/cjs/constants');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-coverage"
+  ],
+  "framework": "@storybook/vue3",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true,
+    interactionsDebugger: true
+  },
+  async viteFinal(config) {
+    config.plugins.push(istanbul({
+      "extension": [
+        ".vue"
+      ]
+    }));
+    // customize the Vite config here
+    return config;
+  },
+}
+
